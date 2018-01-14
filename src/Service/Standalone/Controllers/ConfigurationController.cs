4a260324142125fa0d025376abfb17037bc2363c
Dmitry A. Grechka <dmitry@grechka.family>
2018-01-14 23:15:19 +0300

    Standalone host for simple FC deployment

---
+++ src/Service/Standalone/Controllers/ConfigurationController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Research.Science.FetchClimate2;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Standalone.Controllers
+{
+    public class ConfigurationController : ApiController
+    {
+        private static IVariableDefinition[] ActiveVariables = {
+            new VariableDefinition("airt","Degrees C","Air temperature near surface")
+        };
+
+        private static IDataSourceDefinition[] ActiveDataSources = {
+            new DataSourceDefinition(1,
+                "Reanalysis",
+                "The NCEP/NCAR Reanalysis 1 project is using a state-of-the-art analysis/forecast system to perform data assimilation using past data from 1948 to the present",
+                "NCEP Reanalysis data provided by the NOAA/OAR/ESRL PSD, Boulder, Colorado, USA, from their Web site at http://www.esrl.noaa.gov/psd/",
+                null, new string[] { "airt" })
+        };
+
+        private static IFetchConfiguration ActiveConfiguration =
+            new FetchConfiguration(DateTime.UtcNow, ActiveDataSources, ActiveVariables);
+
+        // GET api/Configuration
+        public Microsoft.Research.Science.FetchClimate2.Serializable.FetchConfiguration Get()
+        {            
+            var toSerizlize = new Microsoft.Research.Science.FetchClimate2.Serializable.FetchConfiguration(ActiveConfiguration);
+            return toSerizlize;
+        }
+
+        // GET api/Configuration?timestamp=03-Nov-2012%2012:00:00
+        public Microsoft.Research.Science.FetchClimate2.Serializable.FetchConfiguration Get(DateTime timestamp)
+        {
+            try
+            {                
+                var toSerizlize = new Microsoft.Research.Science.FetchClimate2.Serializable.FetchConfiguration(ActiveConfiguration);
+                return toSerizlize;
+            }
+            catch (ArgumentException exc)
+            {
+                throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.BadRequest) 
+                { 
+                    ReasonPhrase = exc.Message
+                });
+            }
+        }
+    }
+}
---
+++ src/Service/Standalone/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Standalone
+{
+    public class Startup
+    {
+        // This method is required by Katana:
+        public void Configuration(IAppBuilder app)
+        {
+            var webApiConfiguration = ConfigureWebApi();
+
+            // Use the extension method provided by the WebApi.Owin library:
+            app.UseWebApi(webApiConfiguration);
+        }
+
+
+        private HttpConfiguration ConfigureWebApi()
+        {
+            var config = new HttpConfiguration();
+            
+            config.Routes.MapHttpRoute(
+                name: "Configuration",
+                routeTemplate: "api/configuration/{timestamp}",
+                defaults: new { controller = "Configuration", timestamp = RouteParameter.Optional }
+            );
+            
+            return config;
+        }
+    }
+}
